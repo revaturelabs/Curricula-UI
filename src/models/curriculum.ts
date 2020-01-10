18db364bd836bb916dbdb336d2deab2070d46259
wes-o <weso.turn@gmail.com>
2020-01-10 12:57:53 -0600

    added visualization.ts

---
+++ src/models/curriculum.ts
@@ -0,0 +1,9 @@
+export class Curriculum {
+    curriculumId: number;
+    curriculumName: string;
+
+    constructor(curriculumId: number, curriculumName: string) {
+        this.curriculumId = curriculumId,
+        this.curriculumName = curriculumName
+    }
+}
