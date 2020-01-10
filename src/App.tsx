a37e4450edccb89058bce7b316a2541353742490
mahmud <mahmudskarim@gmail.com>
2020-01-10 14:26:23 -0500

    Basic setup for the search bar for curriculum is done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { store } from './Store';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { SearchCurriculumComponent } from './components/search-curriculum-component/SearchCurriculumComponent';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Provider store={store}>
         <Router>
           <Switch>
+            <Route path='/search' component={SearchCurriculumComponent} />
             <Route path='/' />
           </Switch>
         </Router>
--- src/components/search-curriculum-component/SearchCurriculumComponent.tsx
+++ src/components/search-curriculum-component/SearchCurriculumComponent.tsx
@@ -1,101 +1,93 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import { createStyles, fade, Theme, makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import { Grid, Paper, makeStyles, Theme, createStyles } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+interface ITestData {
+    curriculum: string;
+}
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            flexGrow: 1,
-        },
-        menuButton: {
-            marginRight: theme.spacing(2),
-        },
-        title: {
-            flexGrow: 1,
-            display: 'none',
-            [theme.breakpoints.up('sm')]: {
-                display: 'block',
-            },
-        },
-        search: {
-            position: 'relative',
-            borderRadius: theme.shape.borderRadius,
-            backgroundColor: fade(theme.palette.common.white, 0.15),
-            '&:hover': {
-                backgroundColor: fade(theme.palette.common.white, 0.25),
-            },
-            marginLeft: 0,
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                marginLeft: theme.spacing(1),
-                width: 'auto',
-            },
-        },
-        searchIcon: {
-            width: theme.spacing(7),
-            height: '100%',
-            position: 'absolute',
-            pointerEvents: 'none',
+            padding: '2px 4px',
             display: 'flex',
             alignItems: 'center',
-            justifyContent: 'center',
+            width: 400,
         },
-        inputRoot: {
-            color: 'inherit',
+        input: {
+            marginLeft: theme.spacing(1),
+            flex: 1,
         },
-        inputInput: {
-            padding: theme.spacing(1, 1, 1, 7),
-            transition: theme.transitions.create('width'),
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                width: 120,
-                '&:focus': {
-                    width: 200,
-                },
-            },
+        iconButton: {
+            padding: 10,
+        },
+        divider: {
+            height: 28,
+            margin: 4,
         },
     }),
 );
 
-export default function SearchCurriculumComponent() {
+export function SearchCurriculumComponent() {
+
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        edge="start"
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="open drawer"
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography className={classes.title} variant="h6" noWrap>
-                        Material-UI
-          </Typography>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
-                        </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{ 'aria-label': 'search' }}
-                        />
-                    </div>
-                </Toolbar>
-            </AppBar>
+        <div>
+            <br />
+            <Grid container justify="center" >
+                <Paper component="form" className={classes.root}>
+
+                    <Autocomplete
+                        popupIcon={<SearchIcon />}
+                        placeholder="test"
+                        multiple
+                        id="checkboxes-tags-demo"
+                        options={curriculumList}
+                        disableCloseOnSelect
+                        getOptionLabel={(option: ITestData) => option.curriculum}
+                        renderOption={(option: ITestData, { selected }) => (
+                            <React.Fragment>
+                                <Checkbox
+                                    icon={icon}
+                                    checkedIcon={checkedIcon}
+                                    style={{ marginRight: 8 }}
+                                    checked={selected}
+                                />
+                                {option.curriculum}
+                            </React.Fragment>
+                        )}
+                        style={{ width: 500 }}
+                        renderInput={params => (
+                            <TextField
+                                {...params}
+                                variant="outlined"
+                                label="Search Curriculum"
+                                placeholder="Curriculum"
+                                fullWidth
+                            />
+                        )}
+                    />
+                </Paper>
+            </Grid>
         </div>
     );
 }
+
+
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const curriculumList = [
+    { curriculum: 'Java React' },
+    { curriculum: 'Cyber Security' },
+    { curriculum: 'Java Angular' },
+    { curriculum: 'Java' }
+];
