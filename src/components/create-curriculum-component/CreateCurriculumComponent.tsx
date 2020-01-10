d0bea791af53fecb0bcd56024ea93e5776b5650e
Justin Massey <jdm9six@gmail.com>
2020-01-10 13:49:48 -0500

    started create curriculum component

--- src/components/create-curriculum-component/CreateCurriculumComponent.tsx
+++ src/components/create-curriculum-component/CreateCurriculumComponent.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { SkillPillComponent } from '../skill-pill-component/SkillPillComponent';
+import { Input, Form } from 'reactstrap';
 
 export class CreateCurriculumComponent extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
         this.state = {
-            
+            curriculumName: '',
+            filter: ''
         }
     }
+
+    render() {
+        return (
+            <>
+                <Form onSubmit={this.callSubmitCurriculum}>
+                <Input value={this.state.curriculumName} onChange={this.updateCurriculumName} type="text" id="curriculumName" placeholder="Enter a name for the curriculum" required />
+                <Input value={this.state.filter} onChange={this.updateFilter} type="text" id="filter" placeholder="Type to filter..." />
+                <SkillPillComponent />
+                </Form>
+            </>
+        )
+    }
 }
+
