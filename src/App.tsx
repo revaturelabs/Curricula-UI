3e3f6dc4e172c206c528cb7c454cfa6f4b6cc14d
mahmud <mahmudskarim@gmail.com>
2020-01-10 18:29:35 -0500

    cleaned up the app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import SkillComponent from './components/create-skill-component/CreateSkillComponent';
 import { SearchCurriculumComponent } from './components/search-curriculum-component/SearchCurriculumComponent';
-import PopupComponent from './components/popup-component/PopupButtonComponent';
-import CreateCategory from './components/create-category-component/CreateCategoryComponent';
 import { CreateCurriculumComponent } from './components/create-curriculum-component/CreateCurriculumComponent';
 import { CreateCurriculumPageComponent } from './components/create-curriculum-page-component/CreateCurriculumPageComponent';
 
@@ -19,11 +17,9 @@ const App: React.FC = () => {
         <Router>
           <Switch>
             <Route path='/createcurriculum' component={CreateCurriculumComponent} />
-            <Route path= '/createcurriculumpage' component ={CreateCurriculumPageComponent}></Route>
-            <Route path='/testskill' component={SkillComponent} />
+            <Route path='/createcurriculumpage' component={CreateCurriculumPageComponent}></Route>
             <Route path='/search' component={SearchCurriculumComponent} />
-            <Route path='/testpopupbutton' component={PopupComponent}></Route>
-            <Route path='/skill' component={SkillComponent}/>
+            <Route path='/skill' component={SkillComponent} />
             <Route path='/' />
           </Switch>
         </Router>
