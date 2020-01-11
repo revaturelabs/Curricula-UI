40e9c6202169c90c20e1e491756988b190a905e2
mahmud <mahmudskarim@gmail.com>
2020-01-11 13:07:10 -0500

    The visualization page has now been reworked to populate the store with all the visualizations and skills

--- src/components/view-all-viz-component/ViewAllVisualizationsComponent.tsx
+++ src/components/view-all-viz-component/ViewAllVisualizationsComponent.tsx
@@ -1,31 +1,30 @@
 import React from 'react'
 import { Visualization } from '../../models/visualization';
 
-// interface IVizProps {
-//     submitVisualization: (newVisualization: any) => void
-// }
+interface IVisualizationsProps {
+    visualizations: Visualization[]
+    getAllVisualizations: () => void
+    getAllSkills: () => void
+}
 
-export class ViewAllVizComponent extends React.Component<any, any>{
+export class ViewAllVisualizationsComponent extends React.Component<IVisualizationsProps, any>{
     constructor(props: any) {
         super(props);
         this.state = {
 
-        };
+        }
+    }
+
+    async componentDidMount() {
+        this.props.getAllVisualizations()
+        this.props.getAllSkills()
     }
 
     render() {
         return (
             <div>
-
+                <p>Visualization Page :D</p>
             </div>
-            // <ul>
-            //     {this.state.allVisualizations.map((v: Visualization) => {
-            //         return <div>
-            //             <p>{v.visualizationName}</p>
-            //         </div>
-
-            //     })}
-            // </ul>
         )
 
     }
