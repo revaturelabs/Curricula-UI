0e39bb994bbbe6e2f5bcbfcd4dcbcc3ed1aedbab
Jeffrey Chang <you@example.com>
2020-01-13 10:08:00 -0500

    final push from this branch

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import SkillComponent from './components/create-skill-component/CreateSkillComponent';
 import { SearchCurriculumComponent } from './components/search-curriculum-component/SearchCurriculumComponent';
-import { CreateCurriculumComponent } from './components/create-curriculum-component/CreateCurriculumComponent';
-import  CreateCurriculumPageComponent  from './components/create-curriculum-page-component/CreateCurriculumPageContainer';
-import ViewAllVisualizationsContainer from './components/view-all-visualizations-component/ViewAllVisualizationsContainer';
+import PopupComponent from './components/popup-component/PopupButtonComponent';
+import CreateCategory from './components/create-category-component/CreateCategoryComponent';
+import { CreateCurriculumPageComponent } from './components/create-curriculum-page-component/CreateCurriculumPageComponent';
 
 
 
@@ -17,9 +17,9 @@ const App: React.FC = () => {
       <Provider store={store}>
         <Router>
           <Switch>
-            <Route path='/createcurriculumpage' component={CreateCurriculumPageComponent}></Route>
-            <Route path='/skill' component={SkillComponent} />
-            <Route path='/' component={ViewAllVisualizationsContainer} />
+            <Route path='/search' component={SearchCurriculumComponent} />
+            <Route path='/testpopupbutton' component={PopupComponent}></Route>
+            <Route path='/' />
           </Switch>
         </Router>
       </Provider>
