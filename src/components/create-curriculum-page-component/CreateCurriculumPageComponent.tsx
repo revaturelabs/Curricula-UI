1074a57d4c9970cccd82aa1477861955c348e1f6
gao-shan <sam.mosca@outlook.com>
2020-01-12 23:45:47 -0500

    front-end verification to prevent adding double skills to same curriculum; added update state with curriculum name variable functionality

--- src/components/create-curriculum-page-component/CreateCurriculumPageComponent.tsx
+++ src/components/create-curriculum-page-component/CreateCurriculumPageComponent.tsx
@@ -3,7 +3,7 @@ import { Curriculum } from '../../models/curriculum';
 import PopupButtonComponent from '../popup-component/PopupButtonComponent';
 import { Skill } from '../../models/skill';
 import { Category } from '../../models/category';
-import { Button } from '@material-ui/core';
+import { Button, Input, InputLabel } from '@material-ui/core';
 
 
 interface ICreateCurriculumPageProps{
@@ -18,12 +18,34 @@ export class CreateCurriculumPageComponent extends React.Component<ICreateCurric
         this.state = {
             //newCurriculum: new Curriculum(0, '', this.state.skillsToCurriculumArray),
             skillsToCurriculumArray: [new Skill(0,'',new Category(0,''))],
+            newCurriculumName: '',
             allSkillsMap: [new Skill(0,'',new Category(0,''))]
         }
     }
 
+    upCurriculumName = (e: any) => {
+        this.setState({
+            ...this.state,
+            newCurriculumName: e.target.value
+        })
+    }
+
     upSkillsToCurriculumArray = (e: any) => {
-        this.state.skillsToCurriculumArray.push(this.props.allSkillsMap[e - 1])
+        let skillToAdd = this.props.allSkillsMap[e - 1]
+        let currArray = this.state.skillsToCurriculumArray
+        if (currArray.length === 1){
+            currArray.push(skillToAdd)
+        } else if (currArray.length > 1){
+            for (let i = 0; i < currArray.length; i++){
+                if ((currArray[i].skillId === skillToAdd.skillId)){
+                    alert('That skill already exists, Mate!')
+                    break
+               } else if ((i === currArray.length - 1)){
+                    currArray.push(skillToAdd)
+                    break
+                } 
+           }
+        }
         this.setState({
             ...this.state,
         })
@@ -40,6 +62,11 @@ export class CreateCurriculumPageComponent extends React.Component<ICreateCurric
         
         return(
             <>
+                <div>
+                    {/* <InputLabel className="newCurriculumForm" >Curriculum To Create:</InputLabel> */}
+                    <Input className="newCurriculumForm" placeholder="New Curriculum Name" onChange={this.upCurriculumName}/>
+                    <Button className="newCurriculumForm">Create Curriculum {this.state.newCurriculumName}</Button>
+                </div>
                 
                 <ul className="skillToCurriculumList">
                     {this.state.skillsToCurriculumArray.map((e:any) => {
