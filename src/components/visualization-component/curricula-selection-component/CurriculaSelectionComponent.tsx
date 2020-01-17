9c697a55f1c549bb61bb5d77c031985e37bce555
gao-shan <sam.mosca@outlook.com>
2020-01-17 16:32:38 -0500

    rendering visualizations progress; added css rules to component directory; added grid structure to main component

--- src/components/visualization-component/curricula-selection-component/CurriculaSelectionComponent.tsx
+++ src/components/visualization-component/curricula-selection-component/CurriculaSelectionComponent.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Curriculum } from '../../../models/curriculum';
 import { Paper, Button } from '@material-ui/core';
+import '../Visualization.css'
 
 interface ICurriculaSelectionComponentProps {
     curricula: Curriculum[]
@@ -9,20 +10,22 @@ interface ICurriculaSelectionComponentProps {
 
 export class CurriculaSelectionComponent extends React.PureComponent<ICurriculaSelectionComponentProps> {
 
-    render () {
+    render() {
 
         let rows = this.props.curricula.map((curriuclum) => {
-            return( 
-                <div>
-                    <Button onMouseEnter={()=>{this.props.updateActiveCurriculum(curriuclum)}}>{curriuclum.curriculumName}</Button>
-                </div>
+            return (
+            <div>
+                <Button onMouseEnter={() => { this.props.updateActiveCurriculum(curriuclum) }}>{curriuclum.curriculumName}</Button>
+            </div>
             )
         })
 
         return (
-            <Paper elevation={1}>
-                {rows}
-            </Paper>
+            <div>
+                <Paper className="vizCurriculaSection" elevation={1}>
+                    {rows}
+                </Paper>
+            </div>
         )
     }
 }
