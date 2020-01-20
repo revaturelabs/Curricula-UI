cff958131f75045160986cd5075bfd16fadb9115
gao-shan <sam.mosca@outlook.com>
2020-01-20 11:26:40 -0500

    adding copy to clipboard button

--- src/components/view-all-visualizations-component/visualization-link-component/VisualizationLinkComponent.tsx
+++ src/components/view-all-visualizations-component/visualization-link-component/VisualizationLinkComponent.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { Visualization } from '../../../models/visualization';
 import { Link } from 'react-router-dom';
 import CopyToClipboard from 'react-copy-to-clipboard';
-import { Button } from '@material-ui/core';
+import {  } from '@material-ui/core';
+import clipboard from '../../../assests/clipboard.png' 
+
 
 interface IVisualizationLinkComponentProps {
     visualization: Visualization
@@ -17,7 +19,7 @@ export class VisualizationLinkComponent extends React.PureComponent<IVisualizati
                     {process.env.BASE_URL}/visualizations/{this.props.visualization.visualizationName}
                 </Link>
                 <CopyToClipboard text={`${process.env.BASE_URL}/visualizations/${this.props.visualization.visualizationName}`}>
-                    <Button variant="contained" color="primary">Copy to clipboard</Button>
+                    <img src={clipboard} alt="clipboard-source: https://user-images.githubusercontent.com/35342691/34890669-d3a7694e-f7a0-11e7-8ebe-bc926c10c328.png" color="primary" />
                 </CopyToClipboard>
             </div>
 
