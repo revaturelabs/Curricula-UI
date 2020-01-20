9d5c6ee286efc54cf026d0176b06051c9d38b202
gao-shan <sam.mosca@outlook.com>
2020-01-20 16:09:14 -0500

    added dynamic clipboard animation and icon package from material ui; imported icon styles from google material ui with added link import statement in index.html

--- src/components/view-all-visualizations-component/visualization-link-component/VisualizationLinkComponent.tsx
+++ src/components/view-all-visualizations-component/visualization-link-component/VisualizationLinkComponent.tsx
@@ -2,25 +2,31 @@ import React from 'react'
 import { Visualization } from '../../../models/visualization';
 import { Link } from 'react-router-dom';
 import CopyToClipboard from 'react-copy-to-clipboard';
-import {  } from '@material-ui/core';
-import clipboard from '../../../assests/clipboard.png' 
-
+import { Button, Icon } from '@material-ui/core';
+import '../ViewAllVisualizations.css'
 
 interface IVisualizationLinkComponentProps {
     visualization: Visualization
+    updateClipboardIcon: (visualizationId: number) => void
+    clipboard: any
 }
 
 export class VisualizationLinkComponent extends React.PureComponent<IVisualizationLinkComponentProps>{
 
     render() {
         return (
-            <div>
-                <Link to={`/visualizations/${this.props.visualization.visualizationName}`}>
-                    {process.env.BASE_URL}/visualizations/{this.props.visualization.visualizationName}
-                </Link>
-                <CopyToClipboard text={`${process.env.BASE_URL}/visualizations/${this.props.visualization.visualizationName}`}>
-                    <img src={clipboard} alt="clipboard-source: https://user-images.githubusercontent.com/35342691/34890669-d3a7694e-f7a0-11e7-8ebe-bc926c10c328.png" color="primary" />
-                </CopyToClipboard>
+            <div className="visualizationsBlockElement">
+                <Button variant="contained" color="primary">
+                    <Link to={`/visualizations/${this.props.visualization.visualizationName}`}>
+                        {this.props.visualization.visualizationName}
+                    </Link>
+                </Button>
+                <Button >
+                    <CopyToClipboard text={`${process.env.BASE_URL}/visualizations/${this.props.visualization.visualizationName}`}>
+                        <Icon className='clipboard' onClick={()=>{this.props.updateClipboardIcon(this.props.visualization.visualizationId)}}>{this.props.clipboard}</Icon>
+                    </CopyToClipboard>
+                </Button>
+
             </div>
 
         )
