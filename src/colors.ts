cf0bc37491180a055a0c0c90397fc47ac1d63dd5
Kenneth Davis <kennethdavis391@gmail.com>
2020-01-21 11:10:23 -0500

    environment added, colors made constant

--- src/colors.ts
+++ src/colors.ts
@@ -1,4 +1,4 @@
-let colors: string[] = ["white","#27539F", "#01D2A2", "#317EE6", "#01DBD5", "#311885", "#A64AC9", "#F26925"]
+const COLORS: string[] = ["white","#27539F", "#01D2A2", "#317EE6", "#01DBD5", "#311885", "#A64AC9", "#F26925"]
 
-export default colors
+export default COLORS
 
